bd9d9b10566297df44837f4afada8abe37beb0a8
Tom Nagle <tnagle10@gmail.com>
2016-10-25 13:59:49 -0400

    Initial Code

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,28 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            int input = 0;
+            Console.WriteLine("Please enter a number:");
+            input = int.Parse(Console.ReadLine());
+
+
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("You selected 1");
+                    break;
+                case 2:
+                    Console.WriteLine("You selected 2");
+                    break;
+                case 3:
+                    Console.WriteLine("You selected 3");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    break;
+
+            }
         }
     }
 }
